e1a0e031914756aa077ab196512c02f431b4e293
Abhishek <aabhishekbbhardwaj@gmail.com>
2022-11-08 03:44:14 +0530

    Pickedup individual Option from the matric

--- OMR_Main.py
+++ OMR_Main.py
@@ -2,11 +2,13 @@ import cv2
 import numpy as np
 import Utils
 path = "Answer_Sheet_(Filled).png"
-# widthImg = 700
-# heightImg = 800
+widthImg = 700
+heightImg = 700
+questions = 5
+choices = 5
 img = cv2.imread(path)
 # to re-size image
-# img = cv2.resize(img, (widthImg, heightImg))
+img = cv2.resize(img, (widthImg, heightImg))
 # PROCESSING
 imgContours = img.copy()
 imgBiggestContours = img.copy()
@@ -19,6 +21,7 @@ cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10)
 # FIND RECTANGLES
 rectCon= Utils.rectCountour(contours)
 biggestContours = Utils.getCornerPoints(rectCon[0]) # biggest
+print(biggestContours.shape)
 gradePoints = Utils.getCornerPoints(rectCon[2]) # Second biggest
 # print("Biggest Contours: ",biggestContours)
 
@@ -26,12 +29,41 @@ if biggestContours.size != 0 and gradePoints.size !=0:
     cv2.drawContours(imgBiggestContours, biggestContours, -1, (0, 255, 0), 10)
     cv2.drawContours(imgBiggestContours, gradePoints, -1, (255, 0, 0), 10)
 
+    biggestContours = Utils.reorder(biggestContours)
+    gradePoints = Utils.reorder(gradePoints)
 
+    pt1 = np.float32(biggestContours)
+    pt2 = np.float32([[0, 0],[widthImg,0], [0, heightImg], [widthImg,heightImg]])
+    matrix = cv2.getPerspectiveTransform(pt1, pt2)
+    imgWarpColored= cv2.warpPerspective(img, matrix, (widthImg, heightImg))
 
+    ptG1 = np.float32(gradePoints)
+    ptG2 = np.float32([[0, 0],[325,0], [0, 150], [325,150]])
+    matrixG = cv2.getPerspectiveTransform(ptG1, ptG2)
+    imgGrayDisplay= cv2.warpPerspective(img, matrixG, (325, 150))
+    #cv2.imshow("Grade", imgGrayDisplay)
+
+    # APPLY THRESHOLD
+    imgWarpGray = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)
+    imageThresh = cv2.threshold(imgWarpGray, 170, 225, cv2.THRESH_BINARY_INV)[1]
+
+    boxes = Utils.splitBoxes(imageThresh)
+    # cv2.imshow("Boxes", boxes[0])
+    # print(cv2.countNonZero(boxes[0]), cv2.countNonZero(boxes[1]))
+    myPixelVal = np.zeros((questions, choices))
+    countC=0
+    countR=0
+
+    for image in boxes:
+        totalPixels = cv2.countNonZero(image)
+        myPixelVal[countR][countC] = totalPixels
+        countC +=1
+        if(countC == choices):countR +=1; countC=0
+    print(myPixelVal)
 
 imgBlank = np.zeros_like(img)
 imageArray = ([img, imgGray, imgBlur, imgCanny],
-              [imgContours, imgBiggestContours, imgBlank, imgBlank])
+              [imgContours, imgBiggestContours, imgWarpColored, imageThresh])
 imageStacked = Utils.stackImages(imageArray, 0.5)
 
 cv2.imshow("image Stacked", imageStacked)
--- Utils.py
+++ Utils.py
@@ -58,3 +58,25 @@ def getCornerPoints(count):
     approx = cv2.approxPolyDP(count, 0.02*peri, True)
     return approx
 
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), np.int32)
+    add = myPoints.sum(1)
+    # print(add)
+    myPointsNew[0] = myPoints[np.argmin(add)] # [0, 0]
+    myPointsNew[3] = myPoints[np.argmax(add)] # [w, h]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)] # [w, 0]
+    myPointsNew[2] = myPoints[np.argmax(diff)] # [h, 0]
+    # print(diff)
+    return myPointsNew
+
+
+def splitBoxes(img):
+    rows = np.vsplit(img, 5)
+    boxes = []
+    for r in rows:
+        cols = np.hsplit(r, 5)
+        for box in cols:
+            boxes.append(box)
+    return boxes
